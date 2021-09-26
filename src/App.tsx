76d70a50fe1daa3f3967ac109793677b2a6ae4f4
Yash B <yash.brahmbhatt@greenlightconsulting.com>
2021-09-26 14:21:08 -0400

    saving

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {ActivityBuilder, Assign, InvokeWorkflowFile, If, Sequence } from './components/ui'
+import {UiNode, Designer} from './components/designer'
 
+
+
+const test = new Sequence()
+test.AddActivity(new InvokeWorkflowFile())
+test.AddActivity(new Assign())
+var i = new If()
+i.Arguments[3].Value.AddActivity(new InvokeWorkflowFile())
+console.log(i)
+test.AddActivity(i)
+
+//console.log(test)
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Designer />
+          </>
   );
 }
 
